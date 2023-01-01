420885a36be20dc0e89831744e8ed395b3ec6c7d
Elisheva and shani <eswatch2@gmail.com>
2023-01-01 13:16:07 +0200

    check

--- FirstProject/wwwroot/products.js
+++ FirstProject/wwwroot/products.js
@@ -89,8 +89,8 @@ drawCategory = (category) => {
 }
 filterProducts = async () => {
     var name = document.getElementById("nameSearch").value;
-
-    var minPrice = parseInt( document.getElementById("minPrice").value);
+    
+    var minPrice = parseInt(document.getElementById("minPrice").value);
     console.log(minPrice);
     var maxPrice = parseInt(document.getElementById("maxPrice").value);
     var categoryList = document.getElementsByClassName("opt");
@@ -107,7 +107,7 @@ filterProducts = async () => {
         }
     }
    
-    const url = `Api/products/? name=${name}${categoryIds}&minPrice=${minPrice}&maxPrice=${maxPrice}&start=${start}&end=${limit}&dir=${direction}&orderBy=${orderBy}`;
+    const url = `Api/products/? name=${name}${categoryIds}&&minPrice=${minPrice}&maxPrice=${maxPrice}&start=${start}&end=${limit}&dir=${direction}&orderBy=${orderBy}`;
 
     const res = await fetch(url);
     console.log(res);
