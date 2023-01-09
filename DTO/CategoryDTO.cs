eb04ea6f0f163e1f3ec8604cf1453c619a7e7996
Elisheva and shani <eswatch2@gmail.com>
2023-01-09 10:35:53 +0200

    add interface profile

--- DTO/CategoryDTO.cs
+++ DTO/CategoryDTO.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace DTO
 {
-    public class CategoryDTO
+    public class CategoryDTO : Profile
     {
     }
 }
