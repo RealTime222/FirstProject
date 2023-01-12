4280f8757de9e12e950e9eb806610282f4f126dd
Elisheva and shani <eswatch2@gmail.com>
2023-01-12 15:26:46 +0200

    add props

--- DTO/OrderDTO.cs
+++ DTO/OrderDTO.cs
@@ -1,13 +1,26 @@
 ﻿
+using entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DTO
 {
     public class OrderDTO 
     {
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public int Price { get; set; }
+
+        public int UserId { get; set; }
+
+        public int numOfItems { get; set; }
+
+        public string userName { get; set; }
     }
 }
