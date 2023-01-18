1410062a738395fd8508eadbf409020eba08c274
Elisheva and shani <eswatch2@gmail.com>
2023-01-18 21:32:37 +0200

    add interface to rating

---
+++ data_layer/IdataRating.cs
@@ -0,0 +1,11 @@
+﻿using entities;
+
+namespace data_layer
+{
+    public interface IdataRating
+    {
+        List<Rating> Rating { get; set; }
+
+        Task<Rating> AddData(Rating rating);
+    }
+}
--- data_layer/dataRating.cs
+++ data_layer/dataRating.cs
@@ -1,4 +1,5 @@
 ﻿using entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace data_layer
 {
-    public class dataRating
+    public class dataRating : IdataRating
     {
         public List<Rating> Rating { get; set; }
 
@@ -16,8 +17,13 @@ namespace data_layer
         {
             _WebApiProjectContext = webApiProjectContext;
 
+        }
+        public async Task<Rating> AddData(Rating rating)
+        {
 
-
+            await _WebApiProjectContext.Rating.AddAsync(rating);
+            await _WebApiProjectContext.SaveChangesAsync();
+            return rating;
         }
     }
 }
