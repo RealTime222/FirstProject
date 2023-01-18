4bf454a620a025cbad14ab9cf206a0b7f8ad6d35
Elisheva and shani <eswatch2@gmail.com>
2023-01-18 21:21:47 +0200

    ADD modelBuilder.Entity<Rating>

--- data_layer/WebApiProjectContext.cs
+++ data_layer/WebApiProjectContext.cs
@@ -81,6 +81,37 @@ public partial class WebApiProjectContext : DbContext
                 .HasConstraintName("FK__order_ite__PRODU__398D8EEE");
         });
 
+
+        modelBuilder.Entity<Rating>(entity =>
+        {
+            entity.ToTable("RATING");
+
+            entity.Property(e => e.RatingId).HasColumnName("RATING_ID");
+
+            entity.Property(e => e.Host)
+                .HasColumnName("HOST")
+                .HasMaxLength(50);
+
+            entity.Property(e => e.Method)
+                .HasColumnName("METHOD")
+                .HasMaxLength(10)
+                .IsFixedLength();
+
+            entity.Property(e => e.Path)
+                .HasColumnName("PATH")
+                .HasMaxLength(50);
+
+            entity.Property(e => e.RecordDate)
+             .HasColumnName("Record_Date")
+             .HasColumnType("datetime");
+
+            entity.Property(e => e.Referer)
+                .HasColumnName("REFERER")
+                .HasMaxLength(100);
+
+            entity.Property(e => e.UserAgent).HasColumnName("USER_AGENT");
+        });
+
         modelBuilder.Entity<Product>(entity =>
         {
             entity.HasKey(e => e.ProductId).HasName("PK_PRODUCT");
